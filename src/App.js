acad068de44f49acb454ec1639a158cd3d4898bd
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-07-01 23:42:41 +0100

    :rotating_light: Intalando typescript no projectoi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from "react";
+
 
 function App() {
+
   return (
     <>
-    <h1>Hello World</h1>
+     <h1>Hello Context API</h1>
     </>
   );
 }
